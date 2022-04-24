ccf8a89d8240c541ee045d077d1cd124986f42fa
Ivan Malkovskii <103892616+gotikym@users.noreply.github.com>
2022-04-24 13:47:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,22 @@
-﻿using System;
+using System;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Сколько у вас золота, путник?");
-        var gold = Convert.ToInt32(Console.ReadLine());
-        var crystalCost = 150;
+        int gold = Convert.ToInt32(Console.ReadLine());
+        byte crystalCost = 150;
         Console.WriteLine($"Вы можете купить кристаллы в нашей лавке всего по {crystalCost} золотых, берите у нас и не проходите мимо, сколько вам нужно?");
-        var crystalCount = Convert.ToInt32(Console.ReadLine());
-        
+        int crystalCount = Convert.ToInt32(Console.ReadLine());
+
         if ((crystalCost * crystalCount) > gold)
         {
             crystalCount = gold / crystalCost;
             gold = gold % crystalCost;
         }
-        
-        gold = gold - (crystalCost * crystalCount);
+        else
+            gold = gold - (crystalCost * crystalCount);
         Console.WriteLine($"Золото: {gold} Кристаллы: {crystalCount}");
     }
 }
